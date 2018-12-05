42662b7eddbc68adc43e0dfd48e1a48c1cf1734e
abhinaychaudhary <avi9071@gmail.com>
2018-12-05 14:50:50 +0200

    pages created for each page

---
+++ demo/resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="containser">
+    <div class="card-body">
+        <h1 class="display-4">Welcome to my BLOG</h1>
+        <p class="lead">This is the decription</p>
+    </div>
+</div>
+@endsection
