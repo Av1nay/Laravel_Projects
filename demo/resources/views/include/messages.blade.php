a42917b01c567bdd10879ae49e72013fdb0ebb1e
abhinaychaudhary <avi9071@gmail.com>
2018-12-07 14:54:51 +0200

    success messages and alert added

--- demo/resources/views/include/messages.blade.php
+++ demo/resources/views/include/messages.blade.php
@@ -1,23 +1,23 @@
-//show error messages
-//count no. of errors and show respective error as alert
+{{-- show error messages --}}
+{{-- count no. of errors and show respective error as alert --}}
 @if (count($errors)>0)
     @foreach ($errors ->all() as $error)
-        <div class="alert alert-danger">
+        <div class="alert alert-danger mt-3">
             {{$error}}
         </div>
     @endforeach
 @endif
 
-//check session errors
+{{-- check session errors --}}
 
-@if (session(success))
-    <div class="alert alert-success">
+@if (session('success'))
+    <div class="alert alert-success mt-3">
         {{session('success')}}
     </div>
 @endif
 
 @if (session('success'))
-    <div class="alert alert-danger">
+    <div class="alert alert-danger mt-3">
         {{session('error')}}
     </div>
 @endif
