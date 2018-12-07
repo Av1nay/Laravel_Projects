118dd9deb6246f4d11efa53ce35bd7a6d316a711
abhinaychaudhary <avi9071@gmail.com>
2018-12-07 12:47:43 +0200

    new page created for post

--- demo/resources/views/posts/create.blade.php
+++ demo/resources/views/posts/create.blade.php
@@ -2,5 +2,17 @@
 
 @section('content')
     <h1 class="mt-3">Create Post</h1>
-    
+    {!!Form::open(['action'=>'PostsController@store'])!!}
+        <div class="form-group">
+            <h3>{{Form::label('title','Title')}}</h3>
+            {{Form::text('title','',['class'=>'form-control','placeholder'=>'Title of the post here...'])}}
+        </div>
+        <div class="form-group">
+            <h3>{{Form::label('title','Description')}}</h3>
+            {{Form::textarea('description','',['class'=>'form-control','placeholder'=>'Description of the post here...'])}}
+        </div>
+        <div class="form-group">
+            {{Form::submit('Publish',['class'=>'btn btn-primary'])}}
+        </div>
+    {!! Form::close()!!}
 @endsection
