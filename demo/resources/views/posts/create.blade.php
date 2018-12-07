7ec513e14d251a245c4b46f372468e611a1faf7c
abhinaychaudhary <avi9071@gmail.com>
2018-12-07 14:51:34 +0200

    html escaped using {git add resources/views/posts/show.blade.php git add resources/views/posts/show.blade.php}

--- demo/resources/views/posts/create.blade.php
+++ demo/resources/views/posts/create.blade.php
@@ -4,12 +4,12 @@
     <h1 class="mt-3">Create Post</h1>
     {!!Form::open(['action'=>'PostsController@store'])!!}
         <div class="form-group">
-            <h3>{{Form::label('title','Title')}}</h3>
+            {{Form::label('title','Title',['class'=>'h3'])}}
             {{Form::text('title','',['class'=>'form-control','placeholder'=>'Title of the post here...'])}}
         </div>
         <div class="form-group">
-            <h3>{{Form::label('title','Description')}}</h3>
-            {{Form::textarea('description','',['class'=>'form-control','placeholder'=>'Description of the post here...'])}}
+            {{Form::label('title','Description',['class'=>'h3'])}}
+            {{Form::textarea('description','',['id'=>'article-ckeditor','class'=>'form-control','placeholder'=>'Description of the post here...'])}}
         </div>
         <div class="form-group">
             {{Form::submit('Publish',['class'=>'btn btn-primary'])}}
--- demo/resources/views/posts/show.blade.php
+++ demo/resources/views/posts/show.blade.php
@@ -4,5 +4,5 @@
     <h1 class="mt-3">{{$post -> title}}</h1>
     <small class="card-subtitle text-muted">Written on : {{$post -> created_at}}</small>
     <hr>
-    <p class="card-text h4">{{$post -> description}}</p>
+    <p class="card-text h4">{!! $post -> description !!}</p>
 @endsection
