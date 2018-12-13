1e8d388c1c150a468a1d80db821a1243943c63e1
abhinaychaudhary <avi9071@gmail.com>
2018-12-13 13:59:10 +0200

    delete image function added

--- demo/app/Http/Controllers/PostsController.php
+++ demo/app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Post;
 
 class PostsController extends Controller
@@ -173,6 +174,9 @@ class PostsController extends Controller
         if(auth()->user()->id !==$post->user_id){
             return redirect('/posts')->with('error','Oops!! Unathorized Page.');
         }
+        // delete image
+        Storage::delete('/public/cover_images/'.$post->coverImage);
+
         $post->delete();
         return redirect('/posts')->with('success','Post Removed!!');
     }
